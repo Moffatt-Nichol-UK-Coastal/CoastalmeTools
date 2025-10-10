45012cd38ce20d65673ec84354cdc14e2747d4ff
Wilf Chun <48495899+wilfchun@users.noreply.github.com>
2025-10-10 16:58:02 +0100

    Minor bug fixing

--- src/CoastalmeTools/__init__.py
+++ src/CoastalmeTools/__init__.py
@@ -6,15 +6,11 @@ logger = logging.getLogger(__name__)
 if not logger.handlers:
     handler = logging.StreamHandler()
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
 
 from .Cme import *
-# from .files import *
-# from .parse_results import *
-
-# __all__ = ["Cme", "files", "parse_results"]
